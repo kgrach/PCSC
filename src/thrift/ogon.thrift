0dbce5180720d87e80a8025d15d34a1e79ad72da
Kirill Grachev <pllayer@mail.ru>
2024-12-04 19:51:00 +0700

    Add ListReaders method into thrift interface

--- src/thrift/ogon.thrift
+++ src/thrift/ogon.thrift
@@ -1,15 +1,26 @@
 typedef i64     DWORD_RPC;
-typedef i64     CARDCONTEXT_RPC;
+typedef i64     SCARDCONTEXT_RPC;
 typedef i64     LONG_RPC;
+typedef i64     LPDWORD_RPC;
+typedef string  LPSTR_RPC;
+typedef string  LPCSTR_RPC;
 
 struct return_ec {
+  1: LONG_RPC           retValue,
+  2: SCARDCONTEXT_RPC   cardContext
+}
+
+struct return_lr {
   1: LONG_RPC         retValue,
-  2: CARDCONTEXT_RPC  cardContext
+  2: LPSTR_RPC        Readers, 
+  3: LPDWORD_RPC      ReadersLen
 }
 
 service ogon {
    return_ec EstablishContext(1: DWORD_RPC dwScope)
+   return_lr ListReaders(1: SCARDCONTEXT_RPC hContext, 2: LPCSTR_RPC mszGroups);
 }
+
 
 
 
